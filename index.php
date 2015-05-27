57facd3267affd38c60d07ce7e0e4bd6d1e0e683
John J. Locke <john@lockedowndesign.com>
2015-05-27 07:13:18 -0700

    Validate index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
                 <input type="submit" id="submit" class="take-quiz-btn index-btn redglow" value="Take The Quiz" />
             </div>                        
 	    </form>
-	      <div class="share lb"><a href="http://www.facebook.com/sharer.php?u=http://www.metalquiz.lockedowndesign.com/&&p%5bsummary%5d=What%20Metal%20Band%20Are%20You?%20Quiz%20Take%20this%20quiz%20to%20find%20out." title="Share On Facebook" target="_blank"><img src="../imgs/fb.png" alt="Share this on Facebook" /></a></div>
+	      <div class="share lb"><a href="http://www.facebook.com/sharer.php?u=http://www.metalquiz.lockedowndesign.com/&amp;p%5bsummary%5d=What%20Metal%20Band%20Are%20You?%20Quiz%20Take%20this%20quiz%20to%20find%20out." title="Share On Facebook" target="_blank"><img src="../imgs/fb.png" alt="Share this on Facebook" /></a></div>
           <div class="share rb"><a href="http://twitter.com/home/?status=What%20Metal%20Band%20Are%20You?%20Quiz%20Take%20this%20quiz%20to%20find%20out.%20(http://www.metalquiz.lockedowndesign.com)%20+via+@Lockedown_" title="Share On Twitter" target="_blank"><img src="../imgs/tw.png" alt="Share this on Twitter" /></a></div>
           <p class="shareprompt">Share This Quiz</p>
           <p class="designby shadow1"><span>Built by </span><a href="http://www.lockedowndesign.com"><img src="../imgs/ld.png" alt="Micro Website by Lockedown Design" /></a></p>
